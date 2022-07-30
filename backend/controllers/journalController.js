a62fb8b51c0b6465bfc56e9e616940f750fc6f61
antonioWatkins <tonywatkins325@gmail.com>
2022-07-30 12:00:41 -0400

    rest api

--- backend/controllers/journalController.js
+++ backend/controllers/journalController.js
@@ -1,7 +1,12 @@
 const asyncHandler = require('express-async-handler')
+const Journal = require('../models/journalModel')
+
+
 
 const getJournal =  asyncHandler(async(req, res) => {
-  res.status(200).json({message: 'get jouornal'})
+
+  const journal = await Journal.find()
+  res.status(200).json(journal)
 })
 
 const createJournal = asyncHandler(async(req, res) => {
@@ -10,14 +15,36 @@ const createJournal = asyncHandler(async(req, res) => {
   res.status(400).json({message: 'please write an entry'})
   res.status(200).json({message: 'create jouornal'})
   }
+  const  journal = await Journal.create({
+    text: req.body.text
+  })
+  res.status(200).json(journal)
 })
 
 const updateJournal = asyncHandler(async(req, res) => {
-  res.status(200).json({message: `update journal ${req.params.id}`})
+  const journal =await Journal.findById(req.params.id)
+  
+  if(!journal) {
+    res.status(400)
+    throw new Error('journal not found')
+  }
+
+  const updatedJournal = await Journal.findByIdAndUpdate(req.params.id, req.body,{
+    new: true,
+  })
+  res.status(200).json(`update journal, ${updatedJournal} ${req.params.id}`)
 })
 
-const deleteJournal = asyncHanlder(async(req, res) => {
-  res.status(200).json({message: `delete journal ${req.params.id}`})
+const deleteJournal = asyncHandler(async(req, res) => {
+
+  const journal =await Journal.findById(req.params.id)
+  
+  if(!journal) {
+    res.status(400)
+    throw new Error('journal not found')
+  }
+  await journal.remove()
+  res.status(200).json({id: `delete journal ${req.params.id}`})
 })
 
 module.exports = {
---
+++ backend/models/journalModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const journalSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+      ref: 'User',
+    },
+    text: {
+      type: String,
+      required: [true, 'Please add a text value'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Journal', journalSchema)
